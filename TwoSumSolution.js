14b62ea2e96d9960d8b0b4991bee9359f4934f35
h-miiaah <haim.miiaah5@gmail.com>
2021-12-22 23:58:47 -0500

    solution added

--- TwoSumSolution.js
+++ TwoSumSolution.js
@@ -4,5 +4,13 @@
  * @return {number[]}
  */
  var twoSum = function(nums, target) {
-    
+    let map = new Map();
+    for (let i = 0; i < nums.length; i++){
+        let num1 = nums[i];
+        let num2 = target - num1;
+        if (map.has(num2)){
+            return [i, map.get(num2)];
+        }
+        map.set(num1, i);
+    }
 };
